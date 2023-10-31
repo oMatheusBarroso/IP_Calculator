e666c82a6ee44702262d392592ca3822486f98e6
Matheus Barroso <ma.albarroso@gmail.com>
2023-10-30 23:29:19 -0300

    Reformulação nas lógicas de tratamento do input e identificação de erros

--- main.py
+++ main.py
@@ -1,47 +1,72 @@
 print("~~~~~ Bem-vindo(a) à Calculadora de IPs e Máscaras de Rede ~~~~~\n")
 
-status = False
+#status = False
 
-def solicitation():
+def Solicitation():
     global entry
+    global error_message
+
     print("Insira o IP e o CIDR da máscara no formato IP/CIDR a seguir '121.122.123.124/24'\n")
-    entry = input("Insira seu IP: ").replace(" ","").replace(".","")
+    entry = input("> Insira IP e CIDR: ")\
+        .replace(" ","").replace(".","").replace("\\","/").replace("|","/")
+    
+    if "/" not in entry:
+        error_message = "'/' não encontrada"
+        InvalidIP()
+    
+    Treating()
 
 def InvalidIP():
-    print("\nX ERRO | Formato de IP inválido (QUE NEM VOCÊ) X\n")
-    solicitation()
+    global error_message
 
-def tratamento():
+    print(f"\nX  ERRO | Formato de IP ou CIDR inválido ({error_message}) X\n")
+    Solicitation()
+
+def Treating():
+    #global status
     global ip
     global cidr
-    global status
-    status = False
+    global entry
+    global error_message
+
+    ip, cidr = entry.split("/")
 
     try:
-        ip, cidr = entry.split("/")
+        cidr = int(cidr)
     except ValueError:
-        try:
-            ip, cidr = entry.split("\\")
-        except ValueError:
-            try:
-                ip, cidr = entry.split("|")
-            except ValueError:
-                InvalidIP()
-    else:
-        status = True
+        error_message = "CIDR não é um valor numérico"
+        InvalidIP()
 
-    #if ip.__len__ < 12:
-    #    InvalidIP()
+    if cidr > 30:
+        error_message = "CIDR excede o valor máximo de 30"
+        InvalidIP()
+
+    if len(ip) != 12:
+        error_message = "IP deve ter exatos 12 números"
+        InvalidIP()
+    
+    print(f"\nSeu IP é: {ip}")
+    print(f"Seu CIDR é: {cidr}")
+    
+    x = input("\nDigite Enter")
+
+    #status = True
+
+    MaskCalc()
 
 def MaskCalc():
     mask = "1" * cidr + "0" * (32 - cidr)
-    oct1 = mask[:8]
-    oct2 = mask[8:16]
-    oct3 = mask[16:24]
-    oct4 = mask[24:]
+    mask_oct1 = '0b' + mask[:8]
+    mask_oct2 = '0b' + mask[8:16]
+    mask_oct3 = '0b' + mask[16:24]
+    mask_oct4 = '0b' + mask[24:]
+
+    print("\nMáscara Calculada!")
+    x = input("\nDigite Enter")
 
 #def NetCalc():
 
-while(status == False):
-    solicitation()
-    tratamento()
+#while(status == False):
+Solicitation()
+
+#MaskCalc()
